d6e9781061ab7acbd96f3f30358e9abd2a6928ba
Anastasia Moiseenko <a.anastasiya@ukr.net>
2023-02-13 13:40:39 +0200

    s3 backend configuration for TF

---
+++ s3-backend/s3.tf
@@ -0,0 +1,31 @@
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket        = "${var.project}-s3-backend"
+  force_destroy = true
+
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.s3_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
