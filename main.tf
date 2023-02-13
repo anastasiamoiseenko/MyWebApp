ff77ac9ada10bc80b8b4144262ebf72864d2fb1a
Anastasia Moiseenko <a.anastasiya@ukr.net>
2023-02-13 23:34:46 +0200

    Updated Jenkinsfile-tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region                  = "${var.region}"
-  shared_credentials_files = ["/home/ubuntu/.aws/credentials"]
+  shared_credentials_files = ["$HOME.aws/credentials"]
 }
 
 #-----------network module-----------------
