c96bd16f5bd008686114ab8e78eca6f8b4412796
Anastasia Moiseenko <a.anastasiya@ukr.net>
2023-02-14 12:38:06 +0200

    Updated varriables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,19 @@
 #modules/user-layer/variables.tf
-variable "env" {}
-variable "region" {}
-variable "subnet_cidrs" {}
-variable "vpc_cidr" {}
-variable "instance_type" {}
-variable "volume_size" {}
+variable "env" {
+    default = "webapp"
+}
+variable "region" {
+    default = "eu-central-1"
+}
+variable "subnet_cidrs" {
+    default = ["10.10.1.0/24"]
+}
+variable "vpc_cidr" {
+    default = "10.10.0.0/16"
+}
+variable "instance_type" {
+    default = "t2.micro"
+}
+variable "volume_size" {
+    default = "20"
+}
